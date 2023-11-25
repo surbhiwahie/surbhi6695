33b59dc09667e0e084acdb6af2f778f60dc37455
Surbhi Wahie <surbhiwahie@gmail.com>
2023-11-25 07:00:24 +0530

    Update query_4.sql
    -- Actors History SCD Table Batch Backfill Query (query_4)
    -- Write a "backfill" query that can populate the entire actors_history_scd table in a single query.
    
    -- This is how I will back fill the "actors_history_scd" table, with my batch query:
    -- Step 1: Used a LAG function to track the is_active_last_year column.
    -- Step2: Created a streak column where comparing is_active <> is_active_last_season
    -- Step3: Summing up the streak to see the active/inactive streak
    -- Step4: Inserting the values in the SCD table: by calculating is_active, min(start_year), max(start_year)
    -- Step5: We have this query till 2020, now in the next query of incremental back fill, we will have

---
+++ all_tracks/dimensional_modeling/submission/query_4.sql
@@ -0,0 +1,26 @@
+insert into surbhiwahie.actors_history_scd
+
+WITH lagged as(
+
+select 
+actor,
+is_active ,
+lag(is_active,1) over(partition by actor order by current_year) as is_active_last_year,
+current_year
+from surbhiwahie.actors
+where current_year <= 2020
+),
+streaked as(
+
+select *,
+sum(CASE WHEN is_active <> is_active_last_year THEN 1 ELSE 0 END) over(partition by actor order by current_year) as streak_identifier
+ from lagged)
+ 
+ select 
+ actor,
+ max(is_active) as is_active,
+ min(current_year) as start_year,
+ max(current_year) as end_year,
+ 2020 as current_year
+  from streaked
+  group by actor, streak_identifier
