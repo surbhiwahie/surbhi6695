aacfaf9ed1a2eb5a4d0f7b74f46128cab32c0775
Surbhi Wahie <surbhiwahie@gmail.com>
2023-11-25 06:29:30 +0530

    Create_table_actors_Query1.sql
    Here is a Create table query

---
+++ all_tracks/dimensional_modeling/submission/query_1.sql
@@ -0,0 +1,17 @@
+CREATE TABLE surbhiwahie.actors (
+    actor VARCHAR,
+    actorid VARCHAR,
+    films ARRAY(ROW(
+        film VARCHAR,
+        votes INTEGER,
+        rating INTEGER,
+        filmid VARCHAR
+    )),
+    quality_class VARCHAR,
+    is_active BOOLEAN,
+    current_year INTEGER
+)
+WITH (
+    FORMAT = 'PARQUET',
+    partitioning = ARRAY['current_year']
+)
