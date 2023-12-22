6dcfa10563b05c4782148faac6d549fdbd2e8a37
Surbhi Wahie <surbhiwahie@gmail.com>
2023-12-22 23:10:15 +0530

    Update query_4a.py

---
+++ infrastructure/apache_spark_training/submissions/query_4a.py
@@ -0,0 +1,30 @@
+from pyspark.sql.functions import col, avg
+import os
+from create_spark_session import init_spark
+
+
+def main():
+    spark = init_spark()
+
+
+    # Define the SQL query
+    sql_query = "SELECT * FROM surbhiwahie.joined_bucketed"
+
+    # Fetch data into a DataFrame
+    main_df = spark.sql(sql_query)
+
+    # Show the DataFrame
+    main_df.show()
+
+  # Calculate average kills per game per player
+    Avg_kills_per_game = (
+        main_df.groupBy("player_gamertag")
+        .agg(avg("count").alias("average_kills_per_game"))
+        .orderBy(col("average_kills_per_game").desc())
+    )
+
+    # Show the result
+    Avg_kills_per_game.show(1)
+
+if __name__ == "__main__":
+    main()
