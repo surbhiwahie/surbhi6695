c6057ab52b852f2d8d9504f8965d8153f5ccdffe
Surbhi Wahie <surbhiwahie@gmail.com>
2023-12-23 08:15:53 +0530

    Update query_5a.py

---
+++ infrastructure/apache_spark_training/submissions/query_5a.py
@@ -0,0 +1,21 @@
+from pyspark.sql.functions import col
+import os
+from create_spark_session import init_spark
+
+def main():
+    spark = init_spark()
+
+    df = spark.read.table("surbhiwahie.joined_bucketed")
+    
+    # Version 5a: Partitioned by completion_date and playlist_id
+    partitioned_table_5a = "surbhiwahie.partitioned_table_5a"
+    
+    # Write DataFrame to the new partitioned table
+    df.write.mode("overwrite") \
+    .partitionBy("completion_date", "playlist_id") \
+    .sortWithinPartitions("mapid") \
+    .saveAsTable(partitioned_table_5a)
+    
+
+if __name__ == "__main__":
+    main()
