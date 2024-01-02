fec72a2889f277dde82783f31a09c4a1d9fe8aaa
Surbhi Wahie <surbhiwahie@gmail.com>
2024-01-02 11:01:50 +0530

    Update insert_average_web_events.sql

--- infrastructure/apache_flink_training/submission/insert_average_web_events.sql
+++ infrastructure/apache_flink_training/submission/insert_average_web_events.sql
@@ -1,6 +1,6 @@
 -- Inserting into the table tracking the average number of hits per 5 minute session
 INSERT INTO surbhiwahie.average_web_events_byhost
 select host,
-       AVG(num_hits) as avg_hits_per_5min_session
+       AVG(num_hits) as average_events
 FROM surbhiwahie.processed_events_aggregated_hw
 group by host
