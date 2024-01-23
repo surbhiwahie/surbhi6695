2e3bbefabc6bb31dd00cd9cf373372b700c9ba58
Surbhi Wahie <surbhiwahie@gmail.com>
2024-01-23 11:19:55 -0500

    Update query_5.sql
    Added Grouping sets for query#5 and then calculated the teams that has won the most games.

--- analytics/applying_analytical_patterns/queries/query_5.sql
+++ analytics/applying_analytical_patterns/queries/query_5.sql
@@ -1,32 +1,34 @@
-WITH games AS (
-    SELECT DISTINCT
-        season,
-        game_id,
-        home_team_id,
-        visitor_team_id,
-        home_team_wins,
+WITH
+  Combined AS (
+    SELECT
+      gd.player_id,
+      gd.team_id,
+      g.season,
+      SUM(pts) AS total_points,
+      SUM(
         CASE
-            WHEN home_team_wins = 1 THEN home_team_id
-            ELSE visitor_team_id
-        END AS winning_team
-    FROM bootcamp.nba_games
-),
-
-teams AS (
-    SELECT DISTINCT
-        team_id,
-        team_abbreviation,
-        team_city
-    FROM bootcamp.nba_game_details
-)
-
-SELECT
-    teams.team_abbreviation,
-    COUNT(*) AS total_wins
-FROM games
-LEFT JOIN teams
-    ON games.winning_team = teams.team_id
-GROUP BY team_abbreviation
-ORDER BY COUNT(*) DESC
-
--- LAC	1838
+          WHEN team_id = home_team_id
+          AND home_team_wins = 1 THEN 1
+          WHEN team_id = home_team_id
+          AND home_team_wins = 0 THEN 0
+          WHEN team_id != home_team_id
+          AND home_team_wins = 0 THEN 1
+          ELSE 0
+        END
+      ) AS total_wins
+    FROM
+      bootcamp.nba_game_details gd
+      JOIN bootcamp.nba_games g ON gd.game_id = g.game_id
+    GROUP BY
+      GROUPING SETS (
+        (gd.player_id, gd.team_id),
+        (gd.player_id, g.season),
+        (gd.team_id)
+      )
+  )
+SELECT 
+team_id, total_points, total_wins
+FROM
+  Combined
+  where   total_wins in (
+  select max(total_wins) from Combined where  player_id is null and season is null)
