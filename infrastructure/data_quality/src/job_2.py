886906247b45d25ee7fe6d37919dbb7b36a7ae1f
Surbhi Wahie <surbhiwahie@gmail.com>
2024-01-26 10:28:29 -0500

    job_2.py

--- infrastructure/data_quality/src/job_2.py
+++ infrastructure/data_quality/src/job_2.py
@@ -4,16 +4,27 @@ from pyspark.sql.dataframe import DataFrame
 
 def query_2(output_table_name: str) -> str:
     return f"""
-    <YOUR QUERY HERE>
+    SELECT actorid,
+        CASE 
+            WHEN is_active THEN 1 
+            ELSE 0 
+        END as is_actor_active,
+        CASE WHEN LAG(is_active, 1) OVER (PARTITION BY actorid ORDER BY current_year) THEN 1 
+        ELSE 0 
+        END as act_active_ly,
+        quality_class,
+        current_year
+    FROM actors
+    WHERE current_year <= {year}
     """
 
-def job_2(spark_session: SparkSession, output_table_name: str) -> Optional[DataFrame]:
-  output_df = spark_session.table(output_table_name)
-  output_df.createOrReplaceTempView(output_table_name)
-  return spark_session.sql(query_2(output_table_name))
+def job_2(spark_session: SparkSession, input_dataframe, year:int) -> Optional[DataFrame]:
+  query = query_2(year)
+  input_dataframe.createOrReplaceTempView("actors")
+  return spark_session.sql(query)
 
 def main():
-    output_table_name: str = "<output table name here>"
+    output_table_name: str = "actor_active_ly"
     spark_session: SparkSession = (
         SparkSession.builder
         .master("local")
